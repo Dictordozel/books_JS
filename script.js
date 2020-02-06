a90264f9468dfdae2488093edb2d65ff8cabd068
Ivan Chutkan <alexandrbozhkov1977@gmail.com>
2020-02-06 02:25:29 +0200

    Clean js

--- script.js
+++ script.js
@@ -12,15 +12,10 @@ list[0].insertBefore(elem[6], elem[4]);
 list[0].insertBefore(elem[8], elem[4]);
 list[0].insertBefore(elem[2], elem[10]);
 
-/////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
 
 let books = document.querySelectorAll('aside'),
 book = document.querySelectorAll('div');
-
-//////////////////////////////////////////////////
-// let books = document.querySelectorAll('.books'),
-// book = document.querySelectorAll('.book');
-//////////////////////////////////////////////////
 
 console.log(books, book);
 
@@ -31,16 +26,13 @@ books[0].insertBefore(book[5], book[2]);
 
 book[6].remove();
 
-//////////////////////////////////////////////////
-// let banner = document.querySelectorAll('.adv');
-// banner[0].remove();
-///////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
 
 let title = document.querySelectorAll('a');
 console.log(title);
 title[2].textContent = 'Книга 3. this и Прототипы Объектов';
 
-////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
 
 let newChapter = document.createElement('li');
 newChapter.textContent = 'Глава 8: За пределами ES6';
@@ -48,8 +40,8 @@ console.log(newChapter);
 list[2].appendChild(newChapter);
 list[2].insertBefore(newChapter, elem[26]);
 
-//////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
 
 let newBg = document.querySelector('body');
 console.log(newBg);
-document.body.setAttribute('style', 'background-image: url(./image/you-dont-know-js.jpg)')
+document.body.setAttribute('style', 'background-image: url(./image/you-dont-know-js.jpg)');
